e0f7c261d9433859d6fd317ddf8333f77c4116ac
agmui <65274668+agmui@users.noreply.github.com>
2024-04-10 12:58:05 -0400

    fixed typo

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,7 @@ async function main() {
   console.log(files);
   let exec = require('child_process').exec;
   //exec('cat '+path+files[1],// /home/runner/work/notion2hugo_test/notion2hugo_test/content/Debug-9190bf97e6074f088e2b1efa81052618.md',
-  let w:any = window.open(path+file[1]); //Required full file path.
+  let w:any = window.open(path+files[1]); //Required full file path.
   w.print();
 
 
