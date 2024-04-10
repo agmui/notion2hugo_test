f9e87de25c27780f3bbad70515738f2558b6f44d
agmui <65274668+agmui@users.noreply.github.com>
2024-04-10 13:01:27 -0400

    tried readFile

--- src/index.ts
+++ src/index.ts
@@ -60,8 +60,10 @@ async function main() {
   console.log(files);
   let exec = require('child_process').exec;
   //exec('cat '+path+files[1],// /home/runner/work/notion2hugo_test/notion2hugo_test/content/Debug-9190bf97e6074f088e2b1efa81052618.md',
-  let w:any = window.open(path+files[1]); //Required full file path.
-  w.print();
+  fs.readFile(path+files[1], (err: any, data: any) => {
+    if (err) throw err;
+    console.log(data.toString());
+  });
 
 
 }
